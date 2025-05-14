15a5b31832caf6b14878b17fd53e31b61157e1fa
xdeng21 <xdeng2@wgu.edu>
2025-05-13 21:08:14 -0700

    Wrote code for the cart class in entity package

--- src/main/java/com/d288/demo/entities/Cart.java
+++ src/main/java/com/d288/demo/entities/Cart.java
@@ -1,4 +1,54 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="carts")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="cart_id",nullable = false)
+    private Long id;
+
+    @Column(name="order_tracking_number",nullable = false)
+    private String orderTrackingNumber;
+
+    @Column(name="package_price",nullable = false)
+    private BigDecimal package_price;
+
+    @Column(name="party_size",nullable = false)
+    private int party_size;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusType status;
+
+    @Column(name="create_date",nullable = false)
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name="last_update",nullable = false)
+    @UpdateTimestamp
+    private Date last_update;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id",nullable = false)
+    private Customer customer;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "cart")
+    private Set<CartItem> cartItems= new HashSet<>();
+
 }
