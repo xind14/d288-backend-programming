5e618c55035b4fe8a3580eba8b2fb007181576bc
xdeng21 <xdeng2@wgu.edu>
2025-05-13 21:09:04 -0700

    Wrote code for the division class in entity package

--- src/main/java/com/d288/demo/entities/Division.java
+++ src/main/java/com/d288/demo/entities/Division.java
@@ -1,4 +1,46 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "divisions")
+@Setter
+@Getter
 public class Division {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "division_id")
+    private Long id;
+
+    @Column (name = "division")
+    private String division_name;
+
+    @CreationTimestamp
+    @Column (name = "create_date")
+    private Date create_date;
+
+    @UpdateTimestamp
+    @Column (name = "last_update")
+    private Date last_update;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false, insertable = false, updatable = false)
+    private Country country;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "division")
+    private Set<Customer> customers;
+
+    @Column(name = "country_id")
+    private Long country_id;
+
+
+
 }
