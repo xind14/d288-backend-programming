6ce74b031ee46d1429902d297a40fef9789105e6
xdeng21 <xdeng2@wgu.edu>
2025-05-13 21:08:51 -0700

    Wrote code for the customer class in entity package

--- src/main/java/com/d288/demo/entities/Customer.java
+++ src/main/java/com/d288/demo/entities/Customer.java
@@ -1,4 +1,54 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name="customers")
+@Getter
+@Setter
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id", nullable = false)
+    private Long id;
+
+    @Column(name = "customer_first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "customer_last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postal_code")
+    private String postal_code;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "create_date", nullable = false)
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private Date last_update;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="division_id")
+    private Division divisions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private Set<Cart> carts;
+
 }
