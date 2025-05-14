f907fd2aa45acda9ecd592fcbdecad319aa339fa
xdeng21 <xdeng2@wgu.edu>
2025-05-13 21:08:36 -0700

    Wrote code for the country class in entity package

--- src/main/java/com/d288/demo/entities/Country.java
+++ src/main/java/com/d288/demo/entities/Country.java
@@ -1,4 +1,37 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "countries")
+@Getter
+@Setter
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
+    private Long id;
+
+    @Column(name = "country")
+    private String country_name;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Division> divisions;
+
 }
