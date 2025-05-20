93197b4e5f068eabe5173d1a1773da0cfa13e4d4
xdeng21 <xdeng2@wgu.edu>
2025-05-20 13:10:29 -0700

    Wrote code for the excursion class in entity package

--- src/main/java/com/d288/demo/entities/Excursion.java
+++ src/main/java/com/d288/demo/entities/Excursion.java
@@ -1,4 +1,48 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "excursions")
+@Setter
+@Getter
 public class Excursion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "excursion_id")
+    private Long id;
+
+    @Column(name = "excursion_title")
+    private String excursion_title;
+
+    @Column(name = "excursion_price")
+    private BigDecimal excursion_price
+
+    @Column(name = "image_url")
+    private String image_URL;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @ManyToOne
+    @JoinColumn(name = "vacation_id")
+    private Vacation vacation;
+
+    @ManyToMany(mappedBy = "excurions")
+    Set<CartItem> cart_items;
+
+
 }
