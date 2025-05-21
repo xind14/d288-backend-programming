3f934c00c76b7d4b80d72a4da6310cb0ddead147
xdeng21 <xdeng2@wgu.edu>
2025-05-20 18:19:37 -0700

    Added @NonNull validation to purchase response to match CheckOutServiceImpl logic

--- src/main/java/com/d288/demo/services/PurchaseResponse.java
+++ src/main/java/com/d288/demo/services/PurchaseResponse.java
@@ -1,10 +1,14 @@
 package com.d288.demo.services;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class PurchaseResponse {
+    @NonNull
     private String orderTrackingNumber;
 }
