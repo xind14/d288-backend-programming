bcdcf6d79cb74d48253c2a67fbd0a320ecd8b1a4
xdeng21 <xdeng2@wgu.edu>
2025-05-30 17:58:08 -0700

    Wrote code for the controllers package that includes a REST controller checkout controller class with a post mapping to place orders.

--- src/main/java/com/d288/demo/controllers/CheckoutController.java
+++ src/main/java/com/d288/demo/controllers/CheckoutController.java
@@ -1,4 +1,24 @@
 package com.d288.demo.controllers;
 
+import com.d288.demo.services.CheckoutService;
+import com.d288.demo.services.Purchase;
+import com.d288.demo.services.PurchaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
 public class CheckoutController {
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService){
+        this.checkoutService=checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+
+        PurchaseResponse purchaseResponse=checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
 }
